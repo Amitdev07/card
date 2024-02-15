a5a4840e2ec363a9c81269acea6f1ff3667488fd
Amitdev07 <pushkaramitraj@gmail.com>
2024-02-15 16:27:47 +0530

    new js code

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-let btn = document.getElementById('btn')
-btn.onclick()
+function add(a,b){
+return a+b;
+}
+function a(){
+    return 1;
+}
+function b(){
+    return 2;
+}
 
 
-let val = document.getElementById('input').value
-console.log(val)
-let flag = false;
 
-for(let a =0;a<=val.length-1;a++){
-    if(val.charAt(a)=='@'){
-        flag= true;
-    }
-}
-if(flag==false){
-    console.log('enter correct mail')
-}
-else{
-    console.log('correct value');
-}
